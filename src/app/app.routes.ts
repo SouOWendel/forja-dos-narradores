545a68aef59c97712f38503d5614126bebb96143
SouOWendel <souowendel@gmail.com>
2025-12-06 20:04:11 -0300

    fix(routes): remove duplicate admin route definition and clean up comments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
 	// Defina suas rotas principais aqui
 	{ path: 'auth', loadChildren: () => import('./auth/auth.routes').then(m => m.authRoutes) },
+	{ path: 'admin', loadChildren: () => import('./admin/admin.routes').then(m => m.adminRoutes) },
 	{ path: '', loadChildren: () => import('./blog/blog.routes').then(m => m.blogRoutes) },
-	{ path: '', redirectTo: 'blog', pathMatch: 'full' },
-	//{ path: 'admin', loadChildren: () => import('./admin/admin.routes').then(m => m.adminRoutes) }
+	{ path: '', redirectTo: 'blog', pathMatch: 'full' }
 ];
