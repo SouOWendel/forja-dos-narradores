b75cda5e8a289840b5baf3b2d60afad01b101583
SouOWendel <souowendel@gmail.com>
2025-12-07 21:40:43 -0300

    fix(hero): update hero component layout for better responsiveness

--- src/app/shared/components/hero/hero.ts
+++ src/app/shared/components/hero/hero.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   template: `
     <section class="forja-hero flex bg-gray-100 py-20 px-4 flex-col md:flex-row
 		 items-center justify-evenly space-y-8 md:space-y-0 md:space-x-10 z-8">
-			<div class="container px-4 text-center">
+			<div class="max-w-7xl px-4 text-center">
 				<h2 class="text-5xl mb-4 text-left">A Faísca da Sua História</h2>
 				<p class="text-xl mb-8 text-left w-130">Nos bastidores de todo jogo de sucesso, existe um grupo de narradores dedicados,
 				de criadores que transformaram uma simples ideia em uma lenda. Na Forja dos Narradores, investigamos o fenômeno
@@ -21,9 +21,6 @@ import { Component } from '@angular/core';
 	.forja-hero { height: 350px; }
 	.forja-hero h2 { font-family: 'DM Serif Display', serif; }
 	.forja-hero p { font-family: 'DM Sans', sans-serif; }
-	.forja-hero div.container {
-		max-width: 600px;
-	}
 	.forja-hero-image img {
 		margin-top: 220px;
 		max-width: 500px;
